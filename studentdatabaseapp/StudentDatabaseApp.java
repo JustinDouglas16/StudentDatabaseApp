de83dc048b8e7a9f342f194584ac2bd9b57786da
blackhathacker101 <justindouglas1738@gmail.com>
2021-11-08 10:07:56 -0300

    first commit

---
+++ studentdatabaseapp/StudentDatabaseApp.java
@@ -0,0 +1,12 @@
+package studentdatabaseapp;
+
+public class StudentDatabaseApp {
+
+	public static void main(String[] args) {
+		Student student = new Student();
+		// ask how many new users we want to add
+		
+		//create n number of new students
+	}
+
+}
